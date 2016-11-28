221ae0a7cb58108678c06c7e5b2b22460b85b32e
Craig Martin <craigmichaelmartin@gmail.com>
2016-11-28 13:19:15 -0500

    add tests for simple and static examples

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,164 @@
 import { SuprematismSmartTextPage } from './app.po';
 
-describe('suprematism-smart-text App', function() {
-  let page: SuprematismSmartTextPage;
+const projectedTextHeight = '54px';
+const headerColor = 'rgba(102,102,102,1)';
+const headerEditableColor = 'rgba(0,137,208,1)';
+const text = 'I ponder of something great. My lungs will fill and deflate. They fill with fire, exhale desire. I know it\'s dire my time today.';
+const textShown = 'I ponder of something great. My lungs will fill and deflate. They fill ...';
 
+
+const assertRestingDisplayMode = function(page, truncatedText = textShown) {
+  expect(page.getDisplayElStyle('visibility')).toEqual('visible');
+  expect(page.getEditElStyle('display')).toEqual('none');
+  expect(page.getProjectedTextElStyle('color')).toEqual(headerColor);
+  expect(page.getEditIconElStyle('visibility')).toEqual('hidden');
+  expect(page.getDisplayTextElText()).toEqual(truncatedText);
+};
+const assertHoveredEditableDisplayMode = function(page) {
+  expect(page.getDisplayElStyle('visibility')).toEqual('visible');
+  expect(page.getEditElStyle('display')).toEqual('none');
+  expect(page.getDisplayTextElStyle('color')).toEqual(headerEditableColor);
+  expect(page.getEditIconElStyle('visibility')).toEqual('visible');
+  expect(page.getEditIconElStyle('color')).toEqual(headerEditableColor);
+};
+const assertInRestingEditMode = function(page) {
+  expect(page.getDisplayElStyle('visibility')).toEqual('hidden');
+  expect(page.getEditElStyle('display')).toEqual('block');
+  expect(page.getEditAreaElStyle('color')).toEqual(headerColor);
+  expect(page.getConfirmIconElStyle('visibility')).toEqual('visible');
+  expect(page.getConfirmIconElStyle('color')).toEqual(headerEditableColor);
+  expect(page.getCancelIconElStyle('visibility')).toEqual('visible');
+  expect(page.getCancelIconElStyle('color')).toEqual(headerEditableColor);
+};
+const assertRowsFeature = function(page) {
+  expect(page.getProjectedTextElStyle('height')).toEqual(projectedTextHeight);
+  expect(page.isEllipsisPresent()).toEqual(true);
+};
+const assertPopoverDisplayed = function(page) {
+  expect(page.isPopoverPresent()).toEqual(true);
+  expect(page.getPopoverText()).toEqual(text);
+  expect(page.getPopoverStyle('color')).toEqual(headerColor);
+};
+
+describe('suprematism-smart-text component', function() {
+  let page: SuprematismSmartTextPage;
   beforeEach(() => {
     page = new SuprematismSmartTextPage();
+    page.setBrowserDimensions(1000, 700);
   });
-
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+  describe('static example', () => {
+    beforeEach(() => {
+      page.navigateTo('static');
+    });
+    describe('resting state', () => {
+      it('should have display resting mode set', () => {
+        assertRestingDisplayMode(page);
+      });
+      it('should have rows feature working correctly', () => {
+        assertRowsFeature(page);
+      });
+    });
+    describe('when hovering text', () => {
+      beforeEach(() => {
+        page.hoverOnProjectedText();
+      });
+      it('should remain in display mode', () => {
+        assertRestingDisplayMode(page);
+      });
+      it('should have rows feature working correctly', () => {
+        assertRowsFeature(page);
+      });
+    });
+    describe('when clicking text', () => {
+      beforeEach(() => {
+        page.clickOnProjectedText();
+      });
+      it('should remain in display mode', () => {
+        assertRestingDisplayMode(page);
+      });
+      it('should have rows feature working correctly', () => {
+        assertRowsFeature(page);
+      });
+    });
+    describe('when hovering ellipsis', () => {
+      beforeEach(() => {
+        page.hoverOnEllipsis();
+      });
+      it('should correctly display popover', () => {
+        assertPopoverDisplayed(page);
+      });
+    });
+  });
+  describe('simple example', () => {
+    beforeEach(() => {
+      page.navigateTo('simple');
+    });
+    describe('resting state', () => {
+      it('should have display resting mode set', () => {
+        assertRestingDisplayMode(page);
+      });
+      it('should have rows feature working correctly', () => {
+        assertRowsFeature(page);
+      });
+    });
+    describe('when hovering text', () => {
+      beforeEach(() => {
+        page.hoverOnProjectedText();
+      });
+      it('should change to editable display mode', () => {
+        assertHoveredEditableDisplayMode(page);
+      });
+      it('should have rows feature working correctly', () => {
+        assertRowsFeature(page);
+      });
+    });
+    describe('when clicking text', () => {
+      beforeEach(() => {
+        page.clickOnProjectedText();
+      });
+      describe('resting edit state', () => {
+        it('should change to edit mode', () => {
+          assertInRestingEditMode(page);
+        });
+      });
+      describe('cancel functionality', () => {
+        beforeEach(() => {
+          page.changeEditTextArea('a');
+          page.clickOnCancelIcon();
+        });
+        it('should swtich back to edit mode with unchanged text', () => {
+          assertRestingDisplayMode(page);
+        });
+      });
+      describe('confirm functionality', () => {
+        beforeEach(() => {
+          page.changeEditTextArea('a' + text);
+        });
+        describe('using icon', () => {
+          beforeEach(() => {
+            page.clickOnConfirmIcon();
+          });
+          it('should switch back to edit mode with changed text', () => {
+            assertRestingDisplayMode(page, 'a' + textShown);
+          });
+        });
+        describe('using enter key', () => {
+          beforeEach(() => {
+            page.hitEnterKeyInTextArea();
+          });
+          it('should switch back to edit mode with changed text', () => {
+            assertRestingDisplayMode(page, 'a' + textShown);
+          });
+        });
+      });
+    });
+    describe('when hovering ellipsis', () => {
+      beforeEach(() => {
+        page.hoverOnEllipsis();
+      });
+      it('should correctly display popover', () => {
+        assertPopoverDisplayed(page);
+      });
+    });
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,12 +1,159 @@
-import { browser, element, by } from 'protractor';
+import { browser, element, by, Key } from 'protractor';
+
+const strip = str => str.replace(/['" ]+/g, '');
 
 export class SuprematismSmartTextPage {
 
-  navigateTo() {
-    return browser.get('/');
+  navigateTo(route = '/') {
+    return browser.get(route);
   }
 
-  getParagraphText() {
-    return element(by.css('supre-root h1')).getText();
+  setBrowserDimensions(width, height) {
+    browser.driver.manage().window().setSize(width, height);
+  }
+
+  getProjectedTextEl() {
+    return this.$(`supre-smart-text .header`);
+  }
+
+  getDisplayTextEl() {
+    return this.$(`supre-smart-text .SmartText-DisplayText`);
+  }
+
+  getEllipsisEl() {
+    return this.$(`supre-smart-text .js-shave-char`);
+  }
+
+  getEditIconEl() {
+    return this.$(`supre-smart-text .SmartText-EditIcon`);
+  }
+
+  getConfirmIconEl() {
+    return this.$(`supre-smart-text .SmartText-ConfirmIcon`);
+  }
+
+  getCancelIconEl() {
+    return this.$(`supre-smart-text .SmartText-CancelIcon`);
+  }
+
+  getPopoverEl() {
+    return this.$(`supre-smart-text .js-shave-char supre-popover`);
+  }
+
+  getDisplayEl() {
+    return this.$(`supre-smart-text .SmartText-Display`);
+  }
+
+  getEditEl() {
+    return this.$(`supre-smart-text .SmartText-Edit`);
+  }
+
+  getEditAreaEl() {
+    return this.$(`supre-smart-text .SmartText-EditArea`);
+  }
+
+  getBrowserEllipsisEl() {
+    return browser.findElement(by.css('supre-smart-text .js-shave-char'));
+  }
+
+  getBrowserCancelIcon() {
+    return browser.findElement(by.css('supre-smart-text .SmartText-CancelIcon'));
+  }
+
+  getBrowserConfirmIcon() {
+    return browser.findElement(by.css('supre-smart-text .SmartText-ConfirmIcon'));
+  }
+
+  getBrowserProjectedTextEl() {
+    return browser.findElement(by.css('supre-smart-text .header'));
+  }
+
+  getBrowserEditAreaEl() {
+    return browser.findElement(by.css('supre-smart-text .SmartText-EditArea'));
+  }
+
+  getProjectedTextElStyle(property) {
+    return this.getProjectedTextEl().getCssValue(property).then(strip);
+  }
+
+  getDisplayTextElText() {
+    return this.getDisplayTextEl().getText();
+  }
+
+  getDisplayTextElStyle(property) {
+    return this.getDisplayTextEl().getCssValue(property).then(strip);
+  }
+
+  getEditIconElStyle(property) {
+    return this.getEditIconEl().getCssValue(property).then(strip);
+  }
+
+  getConfirmIconElStyle(property) {
+    return this.getConfirmIconEl().getCssValue(property).then(strip);
+  }
+
+  getCancelIconElStyle(property) {
+    return this.getCancelIconEl().getCssValue(property).then(strip);
+  }
+
+  getDisplayElStyle(property) {
+    return this.getDisplayEl().getCssValue(property).then(strip);
+  }
+
+  getEditElStyle(property) {
+    return this.getEditEl().getCssValue(property).then(strip);
+  }
+
+  getEditAreaElStyle(property) {
+    return this.getEditAreaEl().getCssValue(property).then(strip);
+  }
+
+  getPopoverStyle(property) {
+    return this.getPopoverEl().getCssValue(property).then(strip);
+  }
+
+  getPopoverText() {
+    return this.getPopoverEl().getText();
+  }
+
+  isEllipsisPresent() {
+    return this.getEllipsisEl().isPresent();
+  }
+
+  isPopoverPresent() {
+    return this.getPopoverEl().isPresent();
+  }
+
+  $(path) {
+    return element(by.css(path));
+  }
+
+  hoverOnEllipsis() {
+    return browser.actions().mouseMove(this.getBrowserEllipsisEl()).perform();
+  }
+
+  hoverOnProjectedText() {
+    return browser.actions().mouseMove(this.getBrowserProjectedTextEl()).perform();
+  }
+
+  clickOnProjectedText() {
+    return browser.actions().click(this.getBrowserProjectedTextEl()).perform();
+  }
+
+  clickOnCancelIcon() {
+    return browser.actions().click(this.getBrowserCancelIcon()).perform();
+  }
+
+  clickOnConfirmIcon() {
+    return browser.actions().click(this.getBrowserConfirmIcon()).perform();
+  }
+
+  changeEditTextArea(text) {
+    this.getEditAreaEl().clear();
+    return this.getEditAreaEl().sendKeys(text);
+  }
+
+  hitEnterKeyInTextArea() {
+    return browser.actions().click(this.getBrowserEditAreaEl()).sendKeys(Key.ENTER).perform();
   }
 }
